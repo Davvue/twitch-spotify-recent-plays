e24482da3e7194bd0da7d77a071b77fbdac5909a
David <david.loh18.dl@gmail.com>
2024-02-18 12:15:57 +0100

    Initial commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'img', to: 'img' },
+        { from: 'css', to: 'css' },
+        { from: 'js/vendor', to: 'js/vendor' },
+        { from: '404.html', to: '404.html' },
+      ],
+    }),
+  ],
+});
