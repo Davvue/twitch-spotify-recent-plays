61440fa79ac3a036bc028d58c279e003d6b0b844
David <david.loh18.dl@gmail.com>
2024-02-18 20:12:00 +0100

    Dummy songs

---
+++ js/songs.js
@@ -0,0 +1,37 @@
+const SONGS = [
+  {
+    id: "1",
+    cover: "https://picsum.photos/500?2dhfskj",
+    title: "Test playing",
+    artist: "Test artist 1",
+    playing: true
+  },
+  {
+    id: "2",
+    cover: "https://picsum.photos/500?23ejkd",
+    title: "Test playing",
+    artist: "Test artist 2",
+    playing: true
+  },
+  {
+    id: "3",
+    cover: "https://picsum.photos/500?jsdl",
+    title: "Test playing",
+    artist: "Test artist 3",
+    playing: true
+  },
+  {
+    id: "4",
+    cover: "https://picsum.photos/500?fdhigeur",
+    title: "Test playing",
+    artist: "Test artist 4",
+    playing: true
+  },
+  {
+    id: "5",
+    cover: "https://picsum.photos/500?2ndjsk",
+    title: "Test playing",
+    artist: "Test artist 5",
+    playing: true
+  }
+]
